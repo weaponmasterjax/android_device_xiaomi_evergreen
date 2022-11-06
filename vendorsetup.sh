95addb80b4663e8f34f08f5ff050017aec1cdcf8
Sushrut1101 <guptasushrut@gmail.com>
2022-11-06 21:16:27 +0530

    everpal: Attempt to fix flashlight by using a Symbolic Link
    OrangeFox tries to enable flashlight by echoing the value of OF_FL_PATH(1/2)/max_brightness to OF_FL_PATH(1/2)/brightness.
    A simple way to fix this issue without creating a new node named brighntess for flashlight is to create a symbolic link to the file for enabling flashlight ( /sys/devices/virtual/camera/flash/rear_flash in this case ), name that symbolic link brightness and create a normal file named max_brightness with the value that enables flashlight.
    
    Co-authored-by: Yilliee <m.taha6f@gmail.com>
    Change-Id: I7f3f5b07e8a64d0012a9282279ee242cb412ef4e
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -71,8 +71,10 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 		export OF_HIDE_NOTCH=1
 		export OF_CLOCK_POS=1 # left and right clock positions available
 		export OF_USE_GREEN_LED=0
-		export OF_FL_PATH1="/sys/devices/platform/flashlights_mt6360" # See /init.recovery.mt6785.rc for more information
-
+        
+		# Flashlight
+	    export OF_FL_PATH1="/system/flashlight"
+		
 		# Other OrangeFox configs
 		export OF_ENABLE_LPTOOLS=1
 		export OF_ALLOW_DISABLE_NAVBAR=0
